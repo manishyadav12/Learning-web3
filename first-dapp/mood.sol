b83a42de138f8af49e9be332709f1ff89c6a8638
Manish Yadav <77625361+manishyadav12@users.noreply.github.com>
2023-04-15 22:50:46 +0530

    Create mood.sol

---
+++ first-dapp/mood.sol
@@ -0,0 +1,16 @@
+
+// version
+pragma solidity ^0.8.4;
+
+contract MoodDiary {
+    string mood;
+
+    function setMood(string memory _mood) public {
+        mood = _mood;
+    }
+
+    function getMood() public view returns(string memory){
+        return mood;
+
+    }
+}
